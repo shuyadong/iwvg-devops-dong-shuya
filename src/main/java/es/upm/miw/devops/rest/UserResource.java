f009739ce72c43786190627b61c7f35937fd1c12
Shuya Dong <shuya.dong@alumnos.upm.es>
2026-09-12 10:25:05 +0200

    Finish Feature 3 ref #9

--- src/main/java/es/upm/miw/devops/rest/UserResource.java
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -4,6 +4,7 @@ import es.upm.miw.devops.model.Role;
 import es.upm.miw.devops.model.User;
 import es.upm.miw.devops.model.UsersDatabase;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,12 @@ public class UserResource {
                                    @RequestParam(required = false) Role role,
                                    @RequestParam(required = false) Boolean billable) {
         return this.usersDatabase.findByFilter(active, role, billable);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        if (!this.usersDatabase.deleteById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found: " + id);
+        }
     }
 }
