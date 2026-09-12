9c86d2c9f85a9c6e0b47b36e9be0d7b826a09938
Shuya Dong <shuya.dong@alumnos.upm.es>
2026-09-12 13:30:56 +0200

    Finish Feature 2 test ref #8

--- src/test/java/es/upm/miw/devops/functionaltests/UserResourceFT.java
+++ src/test/java/es/upm/miw/devops/functionaltests/UserResourceFT.java
@@ -1,5 +1,6 @@
 package es.upm.miw.devops.functionaltests;
 
+import es.upm.miw.devops.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -36,5 +39,25 @@ class UserResourceFT {
                 .uri("/user/{id}", "unknown")
                 .exchange()
                 .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testSearchUsersBillableTrue() {
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/user").queryParam("billable", true).build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(User.class)
+                .value(users -> assertThat(users).extracting(User::id).containsExactlyInAnyOrder("1", "2", "4"));
+    }
+
+    @Test
+    void testSearchUsersBillableFalse() {
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/user").queryParam("billable", false).build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(User.class)
+                .value(users -> assertThat(users).extracting(User::id).containsExactlyInAnyOrder("3", "5"));
     }
 }
